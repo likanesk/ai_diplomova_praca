03218f8eb50fd760c2c6e2924ec92be3e52603e4
Adrián Ihring <ihring@stud.uniza.sk>
2025-02-10 14:34:35 +0100

    FE: Added Navbar, Footer, HomePage + Edited page.tsx + Installed React Icons + Deleted unused images

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <a href="https://fastapi.tiangolo.com/">
+              <Image
+                src="/fastapi-icon.png"
+                width={25}
+                height={25}
+                alt="FastAPI Logo"
+              />
+            </a>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              API for medical data
+            </span>
+          </div>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://mit-license.org/"
+                className="hover:underline me-4 md:me-6"
+              >
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/likanesk/ai_diplomova_praca"
+                className="hover:underline"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025 API for medical data
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import {
+  IoBasketOutline,
+  IoCameraOutline,
+  IoFolderOpenOutline,
+  IoHomeOutline,
+  IoLogInOutline,
+  IoServerOutline,
+} from "react-icons/io5";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <Link
+            href="#"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <IoHomeOutline className="w-6 h-6 text-gray-800 dark:text-white mr-1" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Home
+            </span>
+          </Link>
+          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <Link
+              href="#"
+              className="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              <div className="flex">
+                <IoLogInOutline className="w-5 h-5 text-blue-500 dark:text-white mr-1" />
+                Login
+              </div>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  <div className="flex">
+                    <IoBasketOutline className="w-5 h-5 text-gray-800 dark:text-white mr-1" />
+                    Buckets
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  <div className="flex">
+                    <IoServerOutline className="w-5 h-5 text-gray-800 dark:text-white mr-1" />
+                    Datasets
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  <div className="flex">
+                    <IoFolderOpenOutline className="w-5 h-5 text-gray-800 dark:text-white mr-1" />
+                    Classes
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  <div className="flex">
+                    <IoCameraOutline className="w-5 h-5 text-gray-800 dark:text-white mr-1" />
+                    Samples
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
