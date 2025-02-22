7e5652f8088fc8393ed0e110a79d5a2dfe8c42f4
Adrián Ihring <ihring@stud.uniza.sk>
2025-02-22 10:02:27 +0100

    BE: Added authentication - register and login user

--- backend/src/utils/minio_client.py
+++ backend/src/utils/minio_client.py
@@ -1,25 +1,12 @@
 from minio import Minio
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-minio_endpoint = os.getenv("MINIO_ENDPOINT")
-minio_access_key = os.getenv("MINIO_ACCESS_KEY")
-minio_secret_key = os.getenv("MINIO_SECRET_KEY")
-minio_secure = os.getenv("MINIO_SECURE", "false").lower() == "true"
-
-if not minio_endpoint or not minio_access_key or not minio_secret_key:
-    raise ValueError("Minio configuration is missing in the environment variables.")
+from .config import MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_SECURE
 
 client = Minio(
-    minio_endpoint,
-    access_key=minio_access_key,
-    secret_key=minio_secret_key,
-    secure=minio_secure,
+    MINIO_ENDPOINT,
+    access_key=MINIO_ACCESS_KEY,
+    secret_key=MINIO_SECRET_KEY,
+    secure=MINIO_SECURE,
 )
-
 
 def get_minio_client():
     return client
