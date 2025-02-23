bf4b809010644bb28175641f7960e9d4485c7a59
Adrián Ihring <ihring@stud.uniza.sk>
2025-02-23 21:21:05 +0100

    BE: Edited login and register function for better security (password is in body, NOT in query)

--- backend/src/controllers/auth_controller.py
+++ backend/src/controllers/auth_controller.py
@@ -4,30 +4,35 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from fastapi import status
+from pydantic import BaseModel
 from ..utils.config import MINIO_ACCESS_KEY, MINIO_ALIAS, MINIO_SECRET_KEY, MINIO_SERVER
 from ..utils.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
 
-async def register(username: str, password: str):
+class User(BaseModel):
+    username: str
+    password: str
+
+async def register(user: User):
     try:
         setup_minio_alias()
 
-        if user_exists(username):
-            raise HTTPException(status_code=400, detail=f"User {username} already exists.")
+        if user_exists(user.username):
+            raise HTTPException(status_code=400, detail=f"User {user.username} already exists.")
 
         subprocess.run(
-            ["mc", "admin", "user", "add", MINIO_ALIAS, username, password],
+            ["mc", "admin", "user", "add", MINIO_ALIAS, user.username, user.password],
             check=True
         )
 
-        return {"message": f"User {username} was successfully registered."}
+        return {"message": f"User {user.username} was successfully registered."}
 
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"Error registering user: {e}")
 
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    if not authenticate_user(form_data.username, form_data.password):
+async def login(user: User):
+    if not authenticate_user(user.username, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
@@ -37,7 +42,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     # Generate JWT token
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": form_data.username}, expires_delta=access_token_expires
+        data={"sub": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
