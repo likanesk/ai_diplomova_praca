41eddef84324b841c116ea4cd5e03866733c5623
Adrián Ihring <ihring@stud.uniza.sk>
2025-04-01 12:45:22 +0200

    BE - Added check for exactly one JSON file is located in a zip archive (regression, detection)

--- backend/src/validators/detection_validator.py
+++ backend/src/validators/detection_validator.py
@@ -6,12 +6,12 @@ from typing import Dict, List
 class DetectionValidator(CommonValidator):
     """
     This validator is used for validating data for detection. 
-    ROI values in the JSON file must be valid.
+    ROI values in the JSON file must be valid and ZIP must contain exactly one JSON file.
     """
     def validate_detection_data(self, data: List[dict]) -> Dict[str, List]:
         """
         Validates ROI values in the JSON file. 
-        Checks that the ROI values ​​are a list of four integers and that the condition L < R and T < B holds.
+        Checks that the ROI values are a list of four integers and that the condition L < R and T < B holds.
         """
         errors = {"invalid_roi": []}
         for item in data:
@@ -30,23 +30,30 @@ class DetectionValidator(CommonValidator):
         """
         Validates the entire ZIP archive for detection.
         It uses methods from CommonValidator for structure and content validation and adds specific checks for ROI values.
+        Also checks that ZIP contains exactly one JSON file.
         """
         errors = await self.validate_zip_contents(temp_dir, is_regression=False)
         
         if any(errors.values()):
             return errors
         
-        # Load JSON data
+        # Check for exactly one JSON file
         db_folder = os.listdir(temp_dir)[0]
         db_path = os.path.join(temp_dir, db_folder)
         json_files = [f for f in os.listdir(db_path) if f.endswith(".json")]
         
+        if len(json_files) != 1:
+            errors["invalid_structure"].append("ZIP must contain exactly one JSON file")
+            return errors
+        
+        # Load JSON data
         json_path = os.path.join(db_path, json_files[0])
         
         with open(json_path, "r", encoding="utf-8") as f:
             try:
                 data = json.load(f)
             except json.JSONDecodeError:
+                errors["invalid_json"].append("Invalid JSON format")
                 return errors
         
         data_errors = self.validate_detection_data(data)
